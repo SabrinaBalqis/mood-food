ec592708868d353a78286658838ad2cd76ed02a3
Haziq Aiman <muhammad.haziq@agmostudio.com>
2026-06-25 08:30:24 +0800

    fix: angular side issues

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,1 @@
-export const environment = {
-  production: true,
-  supabaseUrl: 'https://YOUR-PROJECT.supabase.co',
-  supabaseKey: 'YOUR_SUPABASE_ANON_KEY',
-  spoonacularApiKey: 'YOUR_SPOONACULAR_KEY',
-  mealDbApiKey: '1'
-};
+export const environment = { production: true, supabaseUrl: "test", supabaseKey: "test", spoonacularApiKey: "test", mealDbApiKey: "1" };
